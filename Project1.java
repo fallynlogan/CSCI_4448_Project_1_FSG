2f066d7bccce1a5fc8971d0ff91772ca4da833e2
fallynlogan <falo7122@colorado.edu>
2020-09-14 12:02:52 -0600

    added more class structure

--- Project1.java
+++ Project1.java
@@ -9,40 +9,102 @@ class HelloWorld {
     }
 }
 
+//1
 class Zoo {
+    //2
     class Animal {
+
+        public void sleep() {
+
+        }
+        //3
         class Pachyderm {
+
+            public void roam() {
+
+            }
+            //4
             class Hippo {
 
+                public void makeNoise() {
+
+                }
+
             }
+            //4
             class Elephant {
 
+                public void makeNoise() {
+
+                }
+
             }
+            //4
             class Rhino {
 
+                public void makeNoise() {
+
+                }
+
             }
         }
+        //3
         class Feline {
+
+            public void roam() {
+
+            }
+            //4
             class Tiger {
 
-            }
-            class Lion {
+                public void makeNoise() {
+
+                }
 
             }
+            //4
+            class Lion {
+
+                public void makeNoise() {
+
+                }
+
+            }
+            //4
             class Leopard {
+
+                public void makeNoise() {
+
+                }
 
             }
         }
+        //3
         class Canine {
-            class Wolf {
+
+            public void roam() {
 
             }
+            //4
+            class Wolf {
+
+                public void makeNoise() {
+
+                }
+
+            }
+            //4
             class Dog {
+
+                public void makeNoise() {
+
+                }
 
             }
         }
     }
 
+    //2
     class Zookeeper {
 
     }
