27a11e3d452a7c211d293aacab3fe3e4f17b7981
ghempy <ghempy@gmail.com>
2020-09-14 17:49:20 +0000

    Adding java file

---
+++ Project1.java
@@ -0,0 +1,10 @@
+//CSCI 4448 Project 1
+//Garrett Hempy
+//Fallyn Logan
+//Serena Evans-Lutterodt
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
