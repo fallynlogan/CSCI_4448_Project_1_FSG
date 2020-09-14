a134510abb17acca0a80d4a471d3057404b74967
ghempy <ghempy@gmail.com>
2020-09-14 21:34:37 +0000

    main

---
+++ src/zoo/Main.java
@@ -0,0 +1,27 @@
+package com.zoo;
+
+public class Main{
+    public static void main(String[] args){
+        Animal zoo[] = new Animal[16];
+        zoo[0] = new Alligator("Allison");
+        zoo[1] = new Alligator("Abraham");
+        zoo[2] = new Dog("Dale");
+        zoo[3] = new Dog("Daisy");
+        zoo[4] = new Elephant("Edward");
+        zoo[5] = new Elephant("Ellie");
+        zoo[6] = new Hippo("Harriet");
+        zoo[7] = new Hippo("Harrison");
+        zoo[8] = new Leopard("Lenny");
+        zoo[9] = new Leopard("Lydia");
+        zoo[10] = new Rhino("Rebecca");
+        zoo[11] = new Rhino("Ron");
+        zoo[12] = new Snake("Sally");
+        zoo[13] = new Snake("Sergio");
+        zoo[14] = new Tiger("Tony");
+        zoo[15] = new Tiger("Tarryn");
+        zoo[16] = new Wolf("Willie");
+        zoo[17] = new Wolf("Wendy");
+        zoo[18] = new Cat("Cathy");
+        zoo[19] = new Cat("Clayton");
+    }
+}
