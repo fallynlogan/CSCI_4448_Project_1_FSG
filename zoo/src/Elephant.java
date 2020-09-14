d978d1c7517adfa727032ff0c82c090ecc6e3869
SerenaLutterodt <47258616+SerenaLutterodt@users.noreply.github.com>
2020-09-14 14:42:25 -0600

    Adding Elephant, Hippo, Lion, and Updates to Wolf

---
+++ zoo/src/Elephant.java
@@ -0,0 +1,12 @@
+package zoo.src;
+
+//File for Elephant extending 
+public class Elephant extends Pachyderm{
+    public Elephant(String name)
+    {
+        super(name);
+    }
+    public void makeNoise() {
+        System.out.print(this.getName() + "OOOOOOOOONJBmkndjsnfjsOJDS");
+    }
+}
--- zoo/src/Wolf.java
+++ zoo/src/Wolf.java
@@ -2,7 +2,6 @@ package zoo.src;
 
 //Note: Canine extends Animal class
 /* Wolf Class
-how to run: java main.java
 */
 public class Wolf extends Canine{
     public Wolf(String name)
