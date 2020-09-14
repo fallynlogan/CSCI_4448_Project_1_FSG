662bb871a5954990851a815c2a08e652389d6941
ghempy <ghempy@gmail.com>
2020-09-14 19:53:44 +0000

    rearranging

---
+++ zoo/src/.ipynb_checkpoints/Animal-checkpoint.java
@@ -0,0 +1,35 @@
+package zoo.src
+
+public class Animal{
+    private String name;
+    private boolean isAsleep = false;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void wakeUp(){
+        isAsleep = false;
+    }
+    
+    public void makeNoise() {
+    
+    }
+    
+    public void eat() {
+    
+    }
+    
+    public void roam() {
+    
+    }
+    
+    public void sleep() {
+        isAsleep = true;
+    }
+}
