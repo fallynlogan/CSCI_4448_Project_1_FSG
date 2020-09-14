8ad0a022c91323d8483645377ff2683005db804c
SerenaLutterodt <47258616+SerenaLutterodt@users.noreply.github.com>
2020-09-14 15:28:54 -0600

    adding updates to file

--- zoo/src/Animal.java
+++ zoo/src/Animal.java
@@ -2,7 +2,8 @@ package zoo.src;
 
 public class Animal{
     private String name;
-    private boolean isAsleep = false;
+    private boolean isAsleep;
+   //Original fucn: private boolean isAsleep = false;
     
     public String getName()
     {
@@ -14,22 +15,40 @@ public class Animal{
     }
     
     public void wakeUp(){
+        if(this.isAsleep){
+            this.isAsleep = false;
+            System.out.println(this.name + " the " +this.getClass().getSimpleName() + " woke up.");
+        }
+        else{
+            System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is already awake.");
+        }
         isAsleep = false;
     }
     
     public void makeNoise() {
+        System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is making a noise.");
     
     }
     
     public void eat() {
+        System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is eating.");
     
     }
     
     public void roam() {
+        System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is roaming around.");
     
     }
     
     public void sleep() {
-        isAsleep = true;
+        if(!this.isAsleep){
+            isAsleep = true;
+            System.out.println(this.name + " the " +this.getClass().getSimpleName() + " has gone to sleep.");
+
+        }
+        else{
+            System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is already asleep.");
+        }
+        
     }
 }
