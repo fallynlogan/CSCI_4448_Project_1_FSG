a831f8c093de7021a2dbdf5eceb324944774e262
fallynlogan <falo7122@colorado.edu>
2020-09-14 20:00:57 -0600

    add readFile

---
+++ readFile.java
@@ -0,0 +1,18 @@
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class CreateFile {
+    public static void main(String[] args) {
+      try {
+        File file_name = new File("our_zoo_.txt");
+        if (file_name.createNewFile()) {
+          System.out.println("File created: " + file_name.getName());
+        } else {
+          System.out.println("File already exists.");
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+    }
+  }
