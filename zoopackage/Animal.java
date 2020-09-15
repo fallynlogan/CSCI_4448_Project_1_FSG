3b7399e9e4e26941b3c67a981bb2e08a2fe8a92b
fallynlogan <falo7122@colorado.edu>
2020-09-15 15:51:22 -0600

    add rand // wip

--- zoopackage/Animal.java
+++ zoopackage/Animal.java
@@ -1,15 +1,17 @@
 package zoopackage;
+import java.util.Random;
 
 public class Animal{
     private String name;
     private boolean isAsleep;
+    public int rand = randomInt();
    //Original fucn: private boolean isAsleep = false;
 
    //constructor
    public Animal(String name){
        this.name = name;
        this.isAsleep = true;
-}
+    }
     
     public String getName()
     {
@@ -18,6 +20,14 @@ public class Animal{
     
     public void setName(String name){
         this.name = name;
+    }
+
+    public int randomInt(){
+        int max = 100;
+        int min = 1;
+        Random randomNum = new Random();
+        int randomNumber = min + randomNum.nextInt(max);
+        return(randomNumber);
     }
     
     public void wakeup(){
@@ -42,15 +52,35 @@ public class Animal{
     }
     
     public void roam() {
-        System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is roaming around.");
-    
+        //When a Dog is given the exercise command by the Zookeeper,there is a 25% chance the dog will dig instead of roaming.
+        if (this.getClass().getSimpleName() == "Dog") {
+            if (0 < rand && rand< 25) {
+                dig(); 
+            }
+        } //When a Pachyderm is given an exercise command, there is a 25% chance it will charge instead of roam.
+        else if (this.getClass().getSimpleName() == "Pachyderm")  {//test this 
+            if (0 < rand && rand< 25) {
+                charge(); 
+            }
+        } else {
+            System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is roaming around.");
+        }
     }
     
     public void sleep() {
         if(!this.isAsleep){
-            isAsleep = true;
-            System.out.println(this.name + " the " +this.getClass().getSimpleName() + " has gone to sleep.");
-
+            if(this.getClass().getSimpleName() == "Cat" || this.getClass().getSimpleName() == "Lion" || this.getClass().getSimpleName() == "Leopard") { //test this
+                System.out.println("This is a feline");
+                //When a Feline is given a sleep command, there is a 30% chance it will roam instead, a 30% chance it will make noise, and a 40% chance it will sleep. 
+                if (0 < rand && rand < 30) {
+                    roam();
+                } else if (30 < rand && rand < 60) {
+                    makeNoise();
+                }
+            } else {
+                isAsleep = true;
+                System.out.println(this.name + " the " +this.getClass().getSimpleName() + " has gone to sleep.");
+            }
         }
         else{
             System.out.println(this.name + " the " +this.getClass().getSimpleName() + " is already asleep.");
--- zoopackage/Zookeeper.java
+++ zoopackage/Zookeeper.java
@@ -1,44 +1,16 @@
 package zoopackage;
-import java.util.Random;
+
 
 public class Zookeeper extends Zooemployee {
     public Zookeeper(String name) {
         super(name);
     }
 
-    public int randomInt(){
-        int max = 100;
-        int min = 1;
-        Random randomNum = new Random();
-        int randomNumber = min + randomNum.nextInt(max);
-        return(randomNumber);
-    }
-
-    public int rand = randomInt(); 
-
     public String exerciseAnimal(Animal[] zoo) {
         String str = this.getName() + " the Zookeeper begins to exercise the animals.";
         System.out.println(str);
         for(int i=0; i<zoo.length ; i++){
-            if(zoo[i].getClass().getSimpleName() == "Dog") {
-                //there is a 25% chance the dog will dig instead of roaming. 
-                if(0 < rand && rand < 25) {
-                    zoo[i].dig();
-                } else {
-                    zoo[i].roam();
-                }
-            } 
-            else if (zoo[i].getClass().getSimpleName() == "Pachyderm") {
-                //there is a 25% chance the Pachyderm will charge instead of roaming. 
-                if(0 < rand && rand < 25) {
-                    zoo[i].charge();
-                } else {
-                    zoo[i].roam();
-                }
-            }
-            else {
             zoo[i].roam();
-            }
         }
         return str;
     }
