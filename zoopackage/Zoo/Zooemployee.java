d40085fb859d1da2059450341e5b72aa073e9635
fallynlogan <falo7122@colorado.edu>
2020-09-14 21:55:50 -0600

    add more

--- zoopackage/Zoo/Zooemployee.java
+++ zoopackage/Zoo/Zooemployee.java
@@ -1,5 +1,24 @@
 package zoopackage.Zoo;
 
 public abstract class Zooemployee {
-    
+    public void wakeAnimalUp(Animal[] zoo) {
+        System.out.println(this.getClass().getSimpleName() + " begins to wake the animals up from their slumber.");
+        for(int i=0; i<zoo.length ; i++){
+            zoo[i].wakeUp();
+        }
+    }
+    public void makeAnimalSleep(Animal[] zoo) {
+        System.out.println(this.getClass().getSimpleName() + "begins to tuck the animals in for the night.");
+        for(int i=0; i<zoo.length ; i++){
+            zoo[i].sleep();
+        }
+    }
+    public void rollCall(Animal[] zoo) {
+        System.out.println("begins to roll call the animals.");
+        for(int i=0; i<zoo.length ; i++){
+            zoo[i].makeNoise();
+        }
+    }
+    public abstract void exerciseAnimal(Animal[] zoo);
+    public abstract void feedAnimal(Animal[] zoo);
 }
--- zoopackage/Zoo/Zookeeper.java
+++ zoopackage/Zoo/Zookeeper.java
@@ -1,24 +1,6 @@
 package zoopackage.Zoo;
 
 public class Zookeeper extends Zooemployee {
-    public void wakeAnimalUp(Animal[] zoo) {
-        System.out.println("Zookeeper begins to wake the animals up from their slumber.");
-        for(int i=0; i<zoo.length ; i++){
-            zoo[i].wakeUp();
-        }
-    }
-    public void makeAnimalSleep(Animal[] zoo) {
-        System.out.println("Zookeeper begins to wake the animals up from their slumber.");
-        for(int i=0; i<zoo.length ; i++){
-            zoo[i].sleep();
-        }
-    }
-    public void rollCall(Animal[] zoo) {
-        System.out.println("Zookeeper begins to roll call the animals.");
-        for(int i=0; i<zoo.length ; i++){
-            zoo[i].makeNoise();
-        }
-    }
     public void exerciseAnimal(Animal[] zoo) {
         System.out.println("Zookeeper begins to exercise the animals.");
         for(int i=0; i<zoo.length ; i++){
