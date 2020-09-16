4413b66da819011bd3af716f76e3e14cadad7cb1
Garrett <gahe0404@colorado.edu>
2020-09-15 20:11:15 -0600

    done?

--- zoopackage/Leopard.java
+++ zoopackage/Leopard.java
@@ -7,6 +7,6 @@ public class Leopard extends Feline{
         super(name);
     }
     public void makeNoise() {
-        System.out.println(this.getName() + "GROWL");
+        System.out.println(this.getName() + " GROWLS");
     }
 }
